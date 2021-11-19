3efb87679e593229afda4ffdc586fde19d56716e
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-20 00:11:37 +0500

    Add files via upload

---
+++ 06 - String/string.py
@@ -0,0 +1,21 @@
+#String
+
+first_name = " Ahmed"
+last_name = "Khatri"
+# bio = "Welcome to my channel!"
+# desc = """
+# I'm teaching you,
+# Python
+# """
+#
+# print("Your first name is :",first_name)
+# print("Your last name is :",last_name)
+# print(bio,end="")
+# print(desc)
+
+print(first_name)
+print("The length of the given string is :",len(first_name))
+print(first_name.lower())
+print(first_name.upper())
+print(first_name.strip()) #remove spacing
+print(last_name.replace("Khatri","Ahmed"))
