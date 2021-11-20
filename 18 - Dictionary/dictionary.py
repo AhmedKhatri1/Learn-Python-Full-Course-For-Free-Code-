92f0904d9f317d27b91b7c09297ce14b3cd1a8ec
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-21 01:15:23 +0500

    Add files via upload

---
+++ 18 - Dictionary/dictionary.py
@@ -0,0 +1,22 @@
+#Dictionary -- key and values (pair)
+
+names = {
+    'Ahmed' : 22,
+    'Ali' : 23,
+    "Akmal" : 24,
+    'Adeel' : 25
+}
+# print(names)
+# print(type(names))
+#
+# print(names['Ahmed'])
+# print(names['Ali'])
+# names['Ali'] = 30
+# print(names['Ali'])
+print(names.keys())
+print(type(names.keys()))
+print(names.values())
+print(type(names.values()))
+print(names.items())
+print(type(names.items()))
+print(names)
