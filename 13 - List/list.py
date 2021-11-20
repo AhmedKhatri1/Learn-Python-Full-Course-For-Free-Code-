b2c669272516b98dd7c984f3ed65039a49ecd6a1
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-20 22:52:41 +0500

    Add files via upload

---
+++ 13 - List/list.py
@@ -0,0 +1,43 @@
+#Listing
+
+college = ["Ak","Bk","Ck","Dk"]
+# print(college)
+# print(type(college))
+#
+# print(college[3])
+# college[1] = "Pk"
+# print(college)
+
+# college.append("Zk")
+# print(college)
+#
+# college.append(10)
+# print(college)
+#
+# college.append(101.9)
+# print(college)
+#
+# college.append(True)
+# print(college)
+
+# print(college[2])
+# college.insert(2,"Yk")
+# print(college)
+
+# print(college)
+# college.remove("Ck")
+# print(college)
+
+nos = [0,1,2,3,4,5,8,11,98,45,24,32,99]
+# # print("The length of the list is :",len(nos))
+# # print(min(nos))
+# # print(max(nos))
+# # nos.sort()
+# # print(nos)
+# nos.reverse()
+# print(nos)
+
+print(nos)
+nos.pop()
+nos.pop()
+print(nos)
