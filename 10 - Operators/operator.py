31458cf9b5b6a724e31c9ab6469044ed5e4c6a26
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-20 17:48:41 +0500

    Add files via upload

---
+++ 10 - Operators/operator.py
@@ -0,0 +1,79 @@
+#Arithmetic , Assignment , Relational , Logical , Unary
+
+# a = 3
+# b = 2
+# print("The value of a is :",a)
+# print("The value of b is :",b)
+# print(3+2)
+# print("The addition of a and b is :",a+b)
+# print("The subtraction of a and b is :",a-b)
+# print("The division of a and b is :",a/b)
+# print("The multiplication of a and b is :",a*b)
+#
+# a = a + 1
+# print(a)
+# b += 1
+# print(b)
+#
+# a = 3
+# a *= 2
+# print(a)
+#
+# # x = 1
+# # y = 2
+# x , y = 1, 2
+# print("The values of x is :",x)
+# print("The values of y is :",y)
+
+#Relational
+
+# a = 1
+# b = 2
+# c = 1
+# # print(a < b)
+# # print(a > b)
+# # print(a==b)
+# # print(a==c)
+# # print(a==a)
+# print(a <= b)
+# print(a >= b)
+# print(a <= c)
+# print(a != b)
+# print(a != c)
+
+#Logical
+
+a = 1
+b = 2
+
+#And
+# print(a < 5 and b < 5)
+# print(a < 5 and b > 5)
+
+#Or
+# print(a < 5 or b < 5)
+# print(a < 5 or b > 5)
+# print(a > 5 or b > 5)
+
+#Not
+
+# x = True
+# print(x)
+# print(type(x))
+# print(not x)
+# y = not x
+# print(y)
+# a = 1
+# b = 2
+# print(a == b)
+# print(a != b)
+
+#Unary
+
+a = 1
+print(a)
+print(-a)
+b = 2
+c = -b
+print(b)
+print(c)
