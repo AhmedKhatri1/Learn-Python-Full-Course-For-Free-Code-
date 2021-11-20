15a71c4cd796a41b08cc4913d96af91e94547a31
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-20 23:46:48 +0500

    Add files via upload

---
+++ 14 - List Slicing/list-slicing.py
@@ -0,0 +1,13 @@
+my_list = [1,2,3,4,5,66,88,99,101,120,875,981,997]
+
+# print(my_list)
+# print(my_list[0:100]) #Total printing of the list till the 100th index
+# print(my_list[1:])
+# print(my_list[:6])
+# print(my_list[6])
+
+# print(my_list[-1])
+# print(my_list[-5:])
+# print(my_list[-8:-5])
+print(my_list[::4]) #gapping of 3 gaps
+print(my_list[::-1]) #reversed method to print list
