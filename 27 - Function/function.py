4f146c7b3f3ae197289fc8ebd11244ef16062f6a
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-22 22:30:01 +0500

    Add files via upload

---
+++ 27 - Function/function.py
@@ -0,0 +1,22 @@
+#Function
+
+# def my_intro():
+#     print("My name is Ahmed")
+#     print("I'm teaching you Python!")
+#
+# def average(no1,no2):
+#     a = print("The average of both no is :",(no1+no2)/2)
+#     # print(a)
+#     return a
+#
+# # my_intro()
+# avg = average(2,2)
+# # print(avg)
+
+def string():
+    '''This is ahmed khatri, welcome to my channel!'''
+    print("Hello guyz!")
+
+print(string.__doc__)
+string()
+
