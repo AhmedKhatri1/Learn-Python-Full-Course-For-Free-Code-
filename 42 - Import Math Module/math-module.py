24fb5ad9075d07c32d769ec1813e676fe20cf3f2
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-24 00:16:16 +0500

    Add files via upload

---
+++ 42 - Import Math Module/math-module.py
@@ -0,0 +1,22 @@
+# a = add(25,5)
+# print(a)
+
+import math as m
+# from math import pow,sqrt
+
+# a = m.sqrt(25)
+# print(a)
+#
+# print(m.floor(2.8))
+# print(m.floor(2.5))
+# print(m.floor(2.1))
+# print(m.ceil(2.8))
+# print(m.ceil(2.5))
+# print(m.ceil(2.1))
+
+# print(m.pow(3,2))
+# print(m.pow(3,3))
+
+# print(m.pi)
+# print(m.e)
+
