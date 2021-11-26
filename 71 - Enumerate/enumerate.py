410a7df2d721e2b8d239a3fb8547282806b08477
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-27 01:27:37 +0500

    Add files via upload

---
+++ 71 - Enumerate/enumerate.py
@@ -0,0 +1,11 @@
+#enumerate
+
+my_list = [1,2,3,"Ahmed",True,'p',"Khatri"]
+
+# ind = 0
+# for i in my_list:
+#     print(i,ind)
+#     ind += 1
+
+for index,i in enumerate(my_list):
+    print("The value is :",i,"and the index no is :",index)
