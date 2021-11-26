963d2c1b028b2e2877694914cf043ae4b2825bb7
Ahmed Khatri <50512044+AhmedKhatri1@users.noreply.github.com>
2021-11-27 01:30:02 +0500

    Add files via upload

---
+++ 80 - if __name__ = __main__/file_1.py
@@ -0,0 +1,16 @@
+#File No 1
+
+def add(no_1,no_2):
+    return no_1 + no_2
+def mul(no_1,no_2):
+    return no_1 * no_2
+def ahmed():
+    print("Hey guyz! Here Ahmed!")
+
+
+if __name__ == '__main__':
+    ahmed()
+    a = add(1, 2)
+    b = mul(2, 2)
+    print(a)
+    print(b)
---
+++ 80 - if __name__ = __main__/file_2.py
@@ -0,0 +1,5 @@
+#File No 2
+import file_1
+
+print("The file name is :",__name__)
+print(file_1.mul(3,3))
